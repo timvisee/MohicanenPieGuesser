6b16caf3286a82d76c1692dd58382e7cdbd0a314
Tim Visée <timvisee@gmail.com>
2016-01-02 22:24:21 +0100

    Added entry to registery database template

--- database/tables/registry.sql
+++ database/tables/registry.sql
@@ -16,7 +16,8 @@ ENGINE=InnoDB
 -- Dumping data for table mohicanenpieguesser.mpg_registry: ~0 rows (approximately)
 /*!40000 ALTER TABLE `mpg_registry` DISABLE KEYS */;
 INSERT INTO `mpg_registry` (`registry_id`, `registry_key`, `registry_value`, `registry_modified_datetime`) VALUES
-	(1, 'client.maxEntries', '5', '2015-09-06 14:10:21');
+	(1, 'client.maxEntries', '5', '2015-09-06 14:10:21'),
+	(2, 'language.default.tag', 'nl-NL', '2015-09-06 14:10:21');
 /*!40000 ALTER TABLE `mpg_registry` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
