9e56e4daab0a13c275bd5f1b3d3f8fe3a8636ed8
Tim Visée <timvisee@gmail.com>
2016-01-04 22:22:12 +0100

    Changed default maximum entries for a device to 3 in registry database template

--- database/tables/registry.sql
+++ database/tables/registry.sql
@@ -16,7 +16,7 @@ ENGINE=InnoDB
 -- Dumping data for table mohicanenpieguesser.mpg_registry: ~0 rows (approximately)
 /*!40000 ALTER TABLE `mpg_registry` DISABLE KEYS */;
 INSERT INTO `mpg_registry` (`registry_id`, `registry_key`, `registry_value`, `registry_modified_datetime`) VALUES
-	(1, 'client.maxEntries', '5', '2015-09-06 14:10:21'),
+	(1, 'client.maxEntries', '3', '2015-09-06 14:10:21'),
 	(2, 'language.default.tag', 'nl-NL', '2015-09-06 14:10:21');
 /*!40000 ALTER TABLE `mpg_registry` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
